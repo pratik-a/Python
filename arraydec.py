e2cb9fba63061f876e5f6ee7c8892b3f2c9bb385
Pratik Awasthi <awasthipratik.98@gmail.com>
2021-06-27 16:43:51 +0530

    Add files via upload

---
+++ arraydec.py
@@ -0,0 +1,20 @@
+from numpy import *
+
+arr = array([1, 2, 3, 4, 5, 6])
+print(arr)
+
+arr1 = linspace(0, 15, 20)
+print(arr1)
+
+arr2 = arange(1, 20, 3)
+print(arr2)
+
+arr3 = logspace(1, 40, 5)
+print(arr3)
+print('%.2f' % arr[4])
+
+arr4 = zeros(5)
+print(arr4)
+
+arr5 = ones(5)
+print(arr5)
