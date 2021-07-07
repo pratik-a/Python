e3a0651d548f0eb62067a61398a9494606cea5f1
Pratik Awasthi <awasthipratik.98@gmail.com>
2021-07-07 12:05:29 +0530

    Add files via upload

---
+++ input.py
@@ -0,0 +1,14 @@
+x = int(input("enter 1 number "))
+y = int(input("enter 2 number "))
+z = x + y
+print(z)
+
+ch = input("enter a character ")
+print(ch)
+print(ch[0])
+
+ch2 = input("enter 2 character ")[0]
+print(ch2)
+
+result = eval(input("enter expression"))
+print(result)
