a56a92b3eac172370cd988c122b4c5e6aabe1f00
Pratik Awasthi <awasthipratik.98@gmail.com>
2021-07-09 15:16:14 +0530

    Add files via upload

---
+++ lambdafun.py
@@ -0,0 +1,15 @@
+from functools import reduce
+
+nums = [2, 45, 65, 34, 67, 98, 25, 100]
+
+evens = list(filter(lambda n: n % 2 == 0, nums))
+
+print(evens)
+
+doubles = list(map(lambda n: n * 2, evens))
+
+print(doubles)
+
+sum = reduce(lambda a, b: a + b, doubles)
+
+print(sum)
