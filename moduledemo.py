f904fa449acd8f2732e9bc3b7e240cd18e0ddf14
Pratik Awasthi <awasthipratik.98@gmail.com>
2021-07-13 14:30:53 +0530

    Add files via upload

---
+++ moduledemo.py
@@ -0,0 +1,16 @@
+from modulefun import *
+
+a = 4
+b = 5
+
+c = add(a, b)
+print("add : ", c)
+
+c = minus(a, b)
+print("minus : ", c)
+
+c = mult(a, b)
+print("mult : ", c)
+
+c = div(a, b)
+print("div : ", c)
