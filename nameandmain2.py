ab4c9cbd3f465986c2881bd5eb20d631cd415e11
Pratik Awasthi <awasthipratik.98@gmail.com>
2021-07-14 00:26:14 +0530

    Add files via upload

---
+++ nameandmain2.py
@@ -0,0 +1,19 @@
+from namenmaincall2 import *
+
+
+def fun1():
+    call()
+    print("from fun 1")
+
+
+def fun2():
+    messg()
+    print("from fun 2")
+
+
+def main():
+    fun1()
+    fun2()
+
+
+main()
