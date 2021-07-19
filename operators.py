20ab59fd297d5ff70023bf9c66b5ed200c389279
Pratik Awasthi <awasthipratik.98@gmail.com>
2021-07-19 19:26:24 +0530

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,32 @@
+x = 2
+y = 3
+x = y + 2
+
+print(x)
+
+x += 2
+
+print(x)
+
+x *= 3
+
+print(x)
+
+a, b = 4, 5
+
+print(a, b)
+
+print(a > b)
+
+print(a < b)
+
+print(a == b)
+
+print(a >= b)
+
+print(a != b)
+
+print(a > 1 and b < 10)
+
+x = True
+x = not x
