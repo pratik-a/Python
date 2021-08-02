789324cf2561aadaec14c394bd7c114c89cba268
Pratik Awasthi <awasthipratik.98@gmail.com>
2021-08-02 23:28:26 +0530

    Add files via upload

---
+++ OOPS/ducktypolymor.py
@@ -0,0 +1,28 @@
+class Pycharm:
+
+    def execute(self):
+        print("compiling")
+        print("running")
+        print("long writing")
+        print("debug")
+
+
+class Jupyter:
+
+    def execute(self):
+        print("compiling")
+        print("running")
+        print("line writing")
+        print("debug")
+
+
+class laptop:
+
+    def code(self):
+        ide.execute()
+
+
+ide = Pycharm()
+
+lap = laptop()
+lap.code()
