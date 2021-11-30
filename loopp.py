971de2ff76e678e3807da1014b38de8dd49159b1
Pratik Awasthi <awasthipratik.98@gmail.com>
2021-11-30 13:29:54 +0530

    Create loopp.py

---
+++ loopp.py
@@ -0,0 +1,13 @@
+def chk():
+    x=0
+    while(x<5):
+        print(x)
+        x=x+1
+    for x in range(5,10):
+        print(x)
+        
+    day=["a","c","b"]
+    for i in day:
+        print(i)
+
+chk()
