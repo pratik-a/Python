b1c0f5c7eaa634d40f5474f8fb4e88f3c3cc887a
Pratik Awasthi <awasthipratik.98@gmail.com>
2021-11-30 13:22:00 +0530

    Create elif.py

---
+++ elif.py
@@ -0,0 +1,14 @@
+def chk():
+    
+    x,y=2,3
+    
+    if(x<y):
+        st="x is less than y"
+    elif(x==y):
+        st="x==y"
+    else:
+         st="y is less than x"   
+    print("x is less than y")
+    
+
+chk()
