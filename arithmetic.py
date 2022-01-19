d5d9d585ad555e6628bc9480e384579c73701ed1
Pratik Awasthi <awasthipratik.98@gmail.com>
2022-01-19 11:31:40 +0530

    Add files via upload

---
+++ arithmetic.py
@@ -0,0 +1,16 @@
+
+def operation(num1,num2):
+
+    print("sum = ",int(num1)+int(num2));
+    print("min = ",int(num1)-int(num2));
+    print("mul = ",int(num1)*int(num2));
+    print("div = ",int(num1)/int(num2));
+
+
+num1=input("first num : ");
+num2=input("first num : ");
+
+if __name__=="__main__":
+    operation(num1,num2);
+
+
