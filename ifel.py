7764795e208660af0068a6fdc762ac6ba2e8f58f
Pratik Awasthi <awasthipratik.98@gmail.com>
2022-01-19 11:31:59 +0530

    Add files via upload

--- ifel.py
+++ ifel.py
@@ -1,6 +1,20 @@
-x = int(input("enter number "))
-if x % 2 == 0:
-    print("even")
-else:
-    print("odd")
-print("end")
+def operation(num1,num2,num3):
+
+    if num1>num2 and num1>num3 :
+       print("num1 is grt");
+    elif num2>num3:
+        print("num2 is grt");
+    else:
+        print("num3 is grt");
+
+
+
+
+
+num1=input("1 num : ");
+num2=input("2 num : ");
+num3=input("3 num : ");
+
+
+if __name__=="__main__":
+    operation(num1,num2,num3);
