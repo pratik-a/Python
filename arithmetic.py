93d7879501bc1f968690112b85df2389b96f8ad8
Pratik Awasthi <awasthipratik.98@gmail.com>
2022-01-19 11:45:18 +0530

    Update arithmetic.py

--- arithmetic.py
+++ arithmetic.py
@@ -1,16 +1,18 @@
 
-def operation(num1,num2):
 
+def operation(num1,num2):
     print("sum = ",int(num1)+int(num2));
     print("min = ",int(num1)-int(num2));
     print("mul = ",int(num1)*int(num2));
     print("div = ",int(num1)/int(num2));
 
-
-num1=input("first num : ");
-num2=input("first num : ");
+def uinput():
+    global num1,num2;
+    num1=input("first num : ");
+    num2=input("first num : ");
 
 if __name__=="__main__":
+    uinput();
     operation(num1,num2);
 
 
